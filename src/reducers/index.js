70bbaddeaf2be28730d76b515c96462c3f66174e
Баринов Сергей Николаевич <barinovsn@OSZNV.ru>
2019-04-16 16:47:02 +0300

    После сбоя

--- src/reducers/index.js
+++ src/reducers/index.js
@@ -1,6 +1,7 @@
 import { combineReducers } from 'redux'
 import ads from './ads'
+import adQueries from './adQueries'
 
-const rootReducer = combineReducers({ ads })
+const rootReducer = combineReducers({ ads, adQueries })
 
 export default rootReducer
