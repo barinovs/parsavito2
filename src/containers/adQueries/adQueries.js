66fc12808300517a5c43ced9744a49df94764215
Баринов Сергей Николаевич <barinovsn@OSZNV.ru>
2019-04-16 16:55:02 +0300

    Пром коммит

--- src/containers/adQueries/adQueries.js
+++ src/containers/adQueries/adQueries.js
@@ -2,15 +2,20 @@ import React from 'react'
 import { connect } from 'react-redux'
 import { bindActionCreators } from 'redux'
 import { getAdQueries } from '../../actions'
+import { API_ENDPOINT } from '../../constants'
 
 class AdQueries extends React.Component{
     constructor(props) {
         super(props)
     }
 
+    componentWillMount() {
+        console.log(API_ENDPOINT);
+    }
+
     render() {
         return(
-            <div></div>
+            <div>Тут будет комбобокс</div>
         )
     }
 
