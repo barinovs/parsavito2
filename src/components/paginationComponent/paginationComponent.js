53bca7e0fd421db8a02f8f42ec4b5ef70efcca01
Баринов Сергей Николаевич <barinovsn@OSZNV.ru>
2019-08-16 15:53:09 +0300

    Ellipsis  в процессе

--- src/components/paginationComponent/paginationComponent.js
+++ src/components/paginationComponent/paginationComponent.js
@@ -4,16 +4,51 @@ import Pagination from 'react-bootstrap/Pagination'
 
 function PaginationComponent ( {countLinks, page, getAdsByPage} ) {
     let items = []
-    for (let number = 1; number <= countLinks; number++) {
-      items.push(
-        <Pagination.Item key={number} active={number === parseInt(page)} onClick={getAdsByPage}>
-          {number}
-        </Pagination.Item>
-      )
+    if (countLinks < 11) {
+        for (let number = 1; number <= countLinks; number++) {
+          items.push(
+            <Pagination.Item key={number} active={number === parseInt(page)} onClick={getAdsByPage}>
+              {number}
+            </Pagination.Item>
+          )
+        }
+    }else{
+        for (let number = 1; number <= 4; number++) {
+            items.push(
+              <Pagination.Item key={number+parseInt(page)} active={number+parseInt(page) === parseInt(page)} onClick={getAdsByPage}>
+                {number+parseInt(page)}
+              </Pagination.Item>
+            )
+        }
     }
-    return (
-        <Pagination>{items}</Pagination>
-    )
+
+
+    if (countLinks < 11) {
+        return (
+            <Pagination>
+                <Pagination.First />
+                <Pagination.Prev />
+                    {items}
+                <Pagination.Next />
+                <Pagination.Last />
+            </Pagination>
+        )
+    }else{
+        return (
+            <Pagination>
+                <Pagination.First />
+                <Pagination.Prev />
+                <Pagination.Item>{1}</Pagination.Item>
+                <Pagination.Ellipsis />
+                    {items}
+                <Pagination.Ellipsis />
+                <Pagination.Item>{countLinks}</Pagination.Item>
+                <Pagination.Next />
+                <Pagination.Last />
+            </Pagination>
+        )
+    }
+
 }
 
 // .propTypes = {
